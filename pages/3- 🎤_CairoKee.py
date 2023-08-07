1335bd5180c26d810573e02b0efdc82caef17994
Mohamed Abdulaziz Eisa <83542891+0xnrous@users.noreply.github.com>
2023-08-07 22:03:56 +0300

    Add files via upload

---
+++ pages/3- 🎤_CairoKee.py
@@ -0,0 +1,28 @@
+#📺 YouTube Channel Data Analytics
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import pandas as pd 
+import os 
+
+st.set_page_config(
+    page_title= '📺 YouTube Channel Data Analytics',
+    page_icon= '🎥',
+    layout="wide")
+st.sidebar.success("Made with ❤️ / \n Created by 0XNROUS")
+
+
+# Header section
+header = st.container()
+with header:
+    st.title("🎤 CairokeeOfficial Channel Analytics")
+    img = st.image("c.jpg", caption=" CairokeeOfficial Channel ", use_column_width=True)
+    
+
+
+
+st.write ("-----------------------------------------------------------------")
+soon = st.container()
+with soon:
+    st.header (" Comming Soon ...")
